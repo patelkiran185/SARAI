c3cb64ee513496b28ed1850d9e3701c8830f46c5
patelkiran185 <kiranpatel502081@gmail.com>
2024-09-13 15:27:56 +0530

    fixed build issues

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'screens/login.dart';
 import 'screens/register.dart';
 import 'screens/home_screen.dart';
 import 'screens/onboarding_screen.dart';
-import 'firebase_options.dart'; 
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
   final bool hasSeenOnboarding;
   final bool isLoggedIn;
 
-  const MyApp({Key? key, required this.hasSeenOnboarding, required this.isLoggedIn}) : super(key: key);
+  const MyApp(
+      {Key? key, required this.hasSeenOnboarding, required this.isLoggedIn})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +33,8 @@ class MyApp extends StatelessWidget {
       title: 'SARAI',
       initialRoute: '/',
       routes: {
-        '/': (context) => SplashScreen(hasSeenOnboarding: hasSeenOnboarding, isLoggedIn: isLoggedIn),
+        '/': (context) => SplashScreen(
+            hasSeenOnboarding: hasSeenOnboarding, isLoggedIn: isLoggedIn),
         '/onboarding': (context) => OnboardingScreen(),
         '/login': (context) => const LoginScreen(),
         '/register': (context) => RegisterScreen(),
@@ -46,16 +49,87 @@ class SplashScreen extends StatefulWidget {
   final bool hasSeenOnboarding;
   final bool isLoggedIn;
 
-  const SplashScreen({Key? key, required this.hasSeenOnboarding, required this.isLoggedIn}) : super(key: key);
+  const SplashScreen(
+      {Key? key, required this.hasSeenOnboarding, required this.isLoggedIn})
+      : super(key: key);
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _navigateToNextScreen();
+  }
+
+  _navigateToNextScreen() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    if (!widget.hasSeenOnboarding) {
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    } else if (widget.isLoggedIn) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+ 
   @override
   Widget build(BuildContext context) {
-    // SplashScreen implementation
-    return Container();
+  return Scaffold(
+  body:Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFA49E9E), Color(0xFFD8B2B2)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: RadialGradient(
+                colors: [Color(0xFFFFFFFF), Color(0xFFC6A8A8)],
+                stops: [0.3, 1.0],
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFA49E9E)),
+                strokeWidth: 6,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
-}
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  }
